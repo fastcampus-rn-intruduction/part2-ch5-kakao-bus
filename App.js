83abf4490b6486a8449ef7c473aa2f34823f7436
daheeahn <deg9810@gmail.com>
2022-11-05 15:08:23 +0900

    [part2-ch5-5-SectionList - 가데이터로 만드는 renderItem] 최종 코드

--- App.js
+++ App.js
@@ -1,44 +1,36 @@
 import { FlatList, SafeAreaView, SectionList, StyleSheet, Text, View } from 'react-native';
+import BusInfo from './src/BusInfo';
+import { COLOR } from './src/color';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>FlatList</Text>
-        <FlatList
-          data={[
-            { busNum: 146 },
-            { busNum: 360 },
-            { busNum: 740 },
-            { busNum: 3412 },
-            { busNum: 1100 },
-            { busNum: 1700 },
-          ]}
-          renderItem={({ item }) => <Text>{item.busNum}</Text>}
-        />
-      </View>
+      <BusInfo
+        isBookmarked={true}
+        onPressBookmark={() => {}}
+        num={146}
+        directionDescription="강남역.강남역사거리"
+        numColor={COLOR.BUS_B}
+      />
 
-      <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>SectionList</Text>
-        <SectionList
-          sections={[
-            {
-              title: "간선버스",
-              data: [{ busNum: 146 }, { busNum: 360 }, { busNum: 740 }],
-            },
-            {
-              title: "지선버스",
-              data: [{ busNum: 3412 }],
-            },
-            {
-              title: "직행버스",
-              data: [{ busNum: 1100 }, { busNum: 1700 }],
-            },
-          ]}
-          renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
-          renderItem={({ item }) => <Text>{item.busNum}</Text>}
-        />
-      </View>
+      {/* <SectionList
+        sections={[
+          {
+            title: "간선버스",
+            data: [{ busNum: 146 }, { busNum: 360 }, { busNum: 740 }],
+          },
+          {
+            title: "지선버스",
+            data: [{ busNum: 3412 }],
+          },
+          {
+            title: "직행버스",
+            data: [{ busNum: 1100 }, { busNum: 1700 }],
+          },
+        ]}
+        renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
+        renderItem={({ item }) => <Text>{item.busNum}</Text>}
+      /> */}
     </SafeAreaView>
   );
 }
--- src/color.js
+++ src/color.js
@@ -7,7 +7,7 @@
  * @property WHITE: 정류소 이름
  * @property BLACK: 'M분 S초' 텍스트
  * @property GRAY_1: 북마크 off, 섹션 백그라운드, 'N번째전' 박스 구분선
- * @property GRAY_2: 정류소 방면 설명, 섹션구분선
+ * @property GRAY_2: 정류소 방면 설명, 섹션구분선, 도착정보 없음
  * @property GRAY_3: 헤더 백그라운드, 버스 방면 설명, 'N번째전' 텍스트, 헤더 북마크 동그라미 버튼의 border, 알람아이콘
  * @property GRAY_4: 섹션타이틀
  */
@@ -20,7 +20,7 @@ export const COLOR = {
   WHITE: "#fefeff", // 정류소 이름
   BLACK: "#333333", // 'M분 S초' 텍스트
   GRAY_1: "#eeeeee", // 북마크 off, 섹션 백그라운드, 'N번째전' 박스 구분선
-  GRAY_2: "#c3c4c6", // 정류소 방면 설명, 섹션구분선
+  GRAY_2: "#c3c4c6", // 정류소 방면 설명, 섹션구분선, 도착정보 없음
   GRAY_3: "#979b9e", // 헤더 백그라운드, 버스 방면 설명, 'N번째전' 텍스트, 헤더 북마크 동그라미 버튼의 borderColor, 알람아이콘
   GRAY_4: "#888789", // 섹션타이틀
 };
